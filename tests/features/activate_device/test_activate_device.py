e3bc57eb44bbddee8d7ec0e239114ce8baa5fcaf
Massimo Biagioli <biagiolimassimo@gmail.com>
2022-10-18 12:35:17 +0200

    Add more test coverage on features

--- tests/features/activate_device/test_activate_device.py
+++ tests/features/activate_device/test_activate_device.py
@@ -1,5 +1,12 @@
-def test_activate_devices(create_devices, client):
+def test_activate_device(create_devices, client):
     response = client.patch("/api/devices/c0a80101-0000-0000-0000-000000000001/activate")
 
     assert response.status_code == 200
     assert response.json()["is_active"] is True
+
+
+def test_activate_non_existing_device(create_devices, client):
+    response = client.patch("/api/devices/c0a80101-0000-0000-0000-000000000999/activate")
+
+    assert response.status_code == 404
+
--- tests/features/deactivate_device/test_deactivate_device.py
+++ tests/features/deactivate_device/test_deactivate_device.py
@@ -1,5 +1,12 @@
-def test_activate_devices(create_activated_device, client):
+def test_activate_device(create_activated_device, client):
     response = client.patch("/api/devices/c0a80101-0000-0000-0000-000000000001/deactivate")
 
     assert response.status_code == 200
     assert response.json()["is_active"] is False
+
+
+def test_activate_non_existing_device(create_activated_device, client):
+    response = client.patch("/api/devices/c0a80101-0000-0000-0000-000000000999/deactivate")
+
+    assert response.status_code == 404
+    
